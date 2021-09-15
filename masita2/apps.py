52b0ac76a91477eaf0aa9dec089ce75ec64d010c
Alan-hub94 <cereal9494@gmai.com>
2021-09-15 16:47:15 -0300

    cupcake2-v2

--- masita2/apps.py
+++ masita2/apps.py
@@ -1,3 +1,5 @@
 from django.apps import AppConfig
 
-
+class masita2(AppConfig):
+    name = 'masita2'
+    verbose_name = 'masita2'
--- masita2/models.py
+++ masita2/models.py
@@ -1,13 +1,20 @@
 from django.db import models
-from django.db.models.fields import CharField
+
 
 # Create your models here.
 
-class Register(models.Model):
-    nombre = models.CharField(max_length=100, null=True, blank=True)
-    email = models.EmailField()
-    description = models.TextField()
-    created = models.DateTimeField(auto_now_add=True)
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name = "Título")
+    description = models.TextField(verbose_name = "Descripción")
+    image = models.ImageField(verbose_name = "Imagen", upload_to="projects")
+    link = models.URLField(null=True, blank=True, verbose_name="Dirección Web")
+    created = models.DateTimeField(auto_now_add=True, verbose_name = "Fecha de creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name = "Fecha de modificación")
+
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
+        ordering = ["-created"]
 
     def __str__(self):
-        return self.nombre
+        return self.title
